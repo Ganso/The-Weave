4003a5047c39a84fcad54f5f6f6b9234468ca2ea
Javier Prieto <spam@ganso.org>
2024-07-25 00:18:13 +0200

    Pattern checks

--- src/dialogs.h
+++ src/dialogs.h
@@ -1,6 +1,8 @@
 #ifndef _DIALOGS_H_
 #define _DIALOGS_H_
 
+#define MAX_TALK_TIME 600   // Default maximum time in conversations (600 ticks, 10 seconds)
+
 void talk(u8, bool, char *, u16); // Make a character talk
 void talk_dialog(u8, bool, u16, u16, u16); // Make a character talk a dialog line
 void split_text(char *, char *, char *, char *); // Split a text in up to three lines
