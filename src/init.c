36a30ad56fe6880266588eb18d2634d4dadcab0c
Javier Prieto <javi@ganso.org>
2024-07-25 22:07:18 +0200

    Pattern check

--- src/init.c
+++ src/init.c
@@ -54,4 +54,5 @@ void initialize(void)
     num_played_notes=0;
     time_since_last_note=0;
     obj_pattern[PTRN_ELECTIC]=(Pattern) {true, {1,2,3,4}, "Electric"};
+    obj_pattern[PTRN_HIDE]=(Pattern) {true, {2,3,4,5}, "Hide"};
 }
