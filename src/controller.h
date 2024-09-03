0f5efa1ed4fcbb59c5586a69d3f077311b382cb5
Javier Prieto <spam@ganso.org>
2024-09-03 10:55:11 +0200

    Controller funcions improvements

--- src/controller.h
+++ src/controller.h
@@ -2,5 +2,8 @@
 #define _CONTROLLER_H_
 
 void joy_check(void); // Check for joystick input
+void joy_check_movement(u16 value); // Check for movement joystick input
+void joy_check_buttons(u16 value); // Check for action button inputs
+
 
 #endif
