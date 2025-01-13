e444d203ddf0c892931f2822e5a6a9809d79ff6b
Javier Prieto <spam@ganso.org>
2025-01-13 18:37:50 +0100

    .

--- src/act_1.c
+++ src/act_1.c
@@ -238,7 +238,7 @@ void act_1_scene_5(void)
     active_character=CHR_linus;
     move_character_instant(CHR_linus, -30, 154);
     move_character_instant(CHR_clio, -30, 154);
-    follow_active_character(CHR_clio, true, 1);
+    follow_active_character(CHR_clio, true, 2);
 
     // Initialize spells
     obj_pattern[PTRN_ELECTRIC].active=true;
