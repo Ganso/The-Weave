6d21cab23dcf0ca2bdd9c8bc6c5642b7163d38ee
Javier Prieto <javi@ganso.org>
2025-03-26 14:24:53 +0100

    Fixes in dialogs

--- src/dialogs.h
+++ src/dialogs.h
@@ -7,7 +7,7 @@
 void talk(u8 nface, bool isinleft, char *text, u16 max_seconds); // Make a character talk
 void talk_dialog(const DialogItem *dialog); // Talk a dialog line
 void split_text(char *input_text, char *line1, char *line2, char *line3); // Split a text in up to three lines
-void print_line(char *text, u16 x, u16 y); // Print a line of text, character by character
+void print_line(char *text, u16 x, u16 y, bool wait_for_frame); // Print a line of text, character by character
 u8 choice(u8 nface, bool isinleft, char **options, u8 num_options, u16 max_seconds); // Show a choice dialog directly
 u8 choice_dialog(const ChoiceItem *choice); // Show a choice dialog from a ChoiceItem
 
