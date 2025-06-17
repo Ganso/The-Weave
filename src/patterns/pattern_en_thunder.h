0fb28f2894f6fccdb981fe4073f590eecb997ba8
Javier Prieto <spam@ganso.org>
2025-06-17 06:25:59 +0200

    Implement HIDE spell

--- src/patterns/pattern_en_thunder.h
+++ src/patterns/pattern_en_thunder.h
@@ -13,4 +13,7 @@ bool enemy_thunder_update(u8 enemyId);
 // Pattern has been countered
 void enemy_thunder_on_counter(u8 enemyId);     // opcional (solo si counterable)
 
+// Cancel thunder without damaging the enemy (used by HIDE)
+void enemy_thunder_cancel(u8 enemyId);
+
 #endif /* PATTERN_EN_THUNDER_H */
