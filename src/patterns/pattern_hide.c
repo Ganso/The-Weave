3195b88587a0cd23d0f87214079dce634bbe5b64
Javier Prieto <javi@ganso.org>
2025-06-17 06:40:54 +0200

    Hide spell (needs fixes)

--- src/patterns/pattern_hide.c
+++ src/patterns/pattern_hide.c
@@ -5,10 +5,13 @@ void player_hide_launch(void)
 {
     bool cancelled_thunder = false;
 
+    dprintf(2,"Hide spell launched");
+
     // If an enemy thunder is active, cancel it and show a hint
     if (combat_state == COMBAT_STATE_ENEMY_EFFECT &&
         combatContext.activePattern == PATTERN_EN_THUNDER)
     {
+        dprintf(2,"Thunder spell cancelled by hide");
         cancelled_thunder = true;
         show_or_hide_interface(false);
         talk_dialog(&dialogs[ACT1_DIALOG3][A1D3_GOOD_IDEA_HIDE]);
@@ -17,6 +20,7 @@ void player_hide_launch(void)
     }
     else if (combatContext.activeEnemy != ENEMY_NONE)
     {
+        dprintf(2,"Some spell cancelled by hide");
         cancel_enemy_pattern(combatContext.activeEnemy);
     }
 
